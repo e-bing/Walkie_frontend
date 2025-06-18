a23fdc6595385d0a3615e3bc39a94d8e837dbb2f
Park Sugwan <456tnrhks@naver.com>
2025-06-18 15:34:38 +0900

    fix : 임시 버튼

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,15 @@
 import React, { useState, useEffect } from 'react';
 import PermissionModal from '@/app/PermissionModal';
 import { fetchTodaySteps } from '@/utils/fitness';
-
+import { useRouter } from 'next/navigation';
 export default function Home() {
-  const [granted, setGranted] = useState(false);
+  const [granted, setGranted] = useState<boolean>(() => {
+    if (typeof window !== 'undefined') {
+      return localStorage.getItem('permissionGranted') === 'true';
+    }
+    return false;
+  });
+  const router = useRouter();
   const [steps, setSteps] = useState<number | null>(null);
 
   useEffect(() => {
@@ -19,7 +25,14 @@ export default function Home() {
   return (
     <>
       {/* 권한 모달 */}
-      {!granted && <PermissionModal onGranted={() => setGranted(true)} />}
+      {!granted && (
+        <PermissionModal
+          onGranted={() => {
+            setGranted(true);
+            localStorage.setItem('permissionGranted', 'true');
+          }}
+        />
+      )}
 
       {/* 권한 허용 후 걸음 수 표시 */}
       <div
@@ -31,6 +44,32 @@ export default function Home() {
         <p className="mt-4 text-6xl">
           {granted ? (steps === null ? '...' : isNaN(steps) ? 'NaN' : steps) : '--'}
         </p>
+      </div>
+      <div className="flex flex-col gap-4 mt-3">
+        <button
+          className="bg-blue-500 text-white px-1 py-2 rounded-md"
+          onClick={() => router.push('/signup/profile')}
+        >
+          회원가입 페이지
+        </button>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded-md"
+          onClick={() => router.push('/signup/gender')}
+        >
+          성별 선택 페이지
+        </button>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded-md"
+          onClick={() => router.push('/signup/terms')}
+        >
+          약관 동의 페이지
+        </button>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded-md"
+          onClick={() => router.push('/login')}
+        >
+          로그인 페이지
+        </button>
       </div>
     </>
   );
