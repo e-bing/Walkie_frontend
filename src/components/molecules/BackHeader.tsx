7eed47683c056c98d8f0ae7afe7f9131722edf72
e-bing <ebing.code@gmail.com>
2025-06-26 21:16:31 +0900

    design : 뒤로가기 헤더 디자인 변경사항 반영

--- src/components/molecules/BackHeader.tsx
+++ src/components/molecules/BackHeader.tsx
@@ -7,9 +7,12 @@ interface BackHeaderProps {
 }
 
 const BackHeader: React.FC<BackHeaderProps> = ({ onBack, title }) => (
-  <header className="fixed top-0 left-0 w-full z-50 bg-white h-12 flex items-center px-4 my-3">
+  <header className="fixed top-0 left-0 w-full z-50 h-12 flex items-center px-4 my-3">
     <BackButton onClick={onBack} />
-    {title && <span className="ml-3 title3">{title}</span>}
+    <div className="flex-1 flex justify-center items-center relative">
+      {title && <span className="labelL text-neutral-800">{title}</span>}
+    </div>
+    <div style={{ width: 40 }} />
   </header>
 );
 
